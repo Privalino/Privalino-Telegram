1326a9366ecbaf29a9c3239ba2ca718ff183ce28
Kolja Lubitz <pinguin999@gmail.com>
2017-06-05 11:43:36 +0200

    First Changes

--- TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
@@ -13,10 +13,10 @@ public class BuildVars {
     public static boolean DEBUG_PRIVATE_VERSION = false;
     public static int BUILD_VERSION = 957;
     public static String BUILD_VERSION_STRING = "3.18";
-    public static int APP_ID = 0; //obtain your own APP_ID at https://core.telegram.org/api/obtaining_api_id
-    public static String APP_HASH = ""; //obtain your own APP_HASH at https://core.telegram.org/api/obtaining_api_id
-    public static String HOCKEY_APP_HASH = "your-hockeyapp-api-key-here";
-    public static String HOCKEY_APP_HASH_DEBUG = "your-hockeyapp-api-key-here";
+    public static int APP_ID = 67270; //obtain your own APP_ID at https://core.telegram.org/api/obtaining_api_id
+    public static String APP_HASH = "a3589d4517fd8c12384fc7bd9c0ac573"; //obtain your own APP_HASH at https://core.telegram.org/api/obtaining_api_id
+    public static String HOCKEY_APP_HASH = "baf18098164e4e1786bd8acb7d9fd406";
+    public static String HOCKEY_APP_HASH_DEBUG = "a3589d4517fd8c12384fc7bd9c0ac573";
     public static String BING_SEARCH_KEY = ""; //obtain your own KEY at https://www.bing.com/dev/en-us/dev-center
     public static String FOURSQUARE_API_KEY = ""; //obtain your own KEY at https://developer.foursquare.com/
     public static String FOURSQUARE_API_ID = ""; //obtain your own API_ID at https://developer.foursquare.com/
