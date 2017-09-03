97ed6fdab7028ef84a88fbef7dbe2223b32a7a8a
Kolja Lubitz <pinguin999@gmail.com>
2017-09-03 14:39:11 +0200

    Fix Crash bei anrufen

--- TMessagesProj/src/main/java/org/telegram/messenger/MessagesController.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/MessagesController.java
@@ -6462,7 +6462,9 @@ public class MessagesController implements NotificationCenter.NotificationCenter
 
             if(updates instanceof TLRPC.TL_updates) {
                 for (TLRPC.Update update : updates.updates) {
-                    processUpdateWithPrivalino(((TLRPC.TL_updateNewMessage) update).message, update.user_id);
+                    if (update instanceof TLRPC.TL_updateNewMessage) {
+                        processUpdateWithPrivalino(((TLRPC.TL_updateNewMessage) update).message, update.user_id);
+                    }
                 }
             }
 
