7ea52ff04a49149b1d0c36c4718a2ccb7e6fc45e
Max <phraine@gmail.com>
2018-02-15 15:08:15 +0100

    Don't show special notifications for blocked media typing

--- TMessagesProj/src/main/java/org/telegram/messenger/MessagesController.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/MessagesController.java
@@ -2570,63 +2570,63 @@ public class MessagesController implements NotificationCenter.NotificationCenter
                 if (user == null) {
                     return;
                 }
-                if (pu.action instanceof TLRPC.TL_sendMessageRecordAudioAction) {
-                    if (lower_id < 0) {
-                        newPrintingStrings.put(key, LocaleController.formatString("IsRecordingAudio", R.string.IsRecordingAudio, getUserNameForTyping(user)));
-                    } else {
-                        newPrintingStrings.put(key, LocaleController.getString("RecordingAudio", R.string.RecordingAudio));
-                    }
-                    newPrintingStringsTypes.put(key, 1);
-                } else if (pu.action instanceof TLRPC.TL_sendMessageRecordRoundAction || pu.action instanceof TLRPC.TL_sendMessageUploadRoundAction) {
-                    if (lower_id < 0) {
-                        newPrintingStrings.put(key, LocaleController.formatString("IsRecordingRound", R.string.IsRecordingRound, getUserNameForTyping(user)));
-                    } else {
-                        newPrintingStrings.put(key, LocaleController.getString("RecordingRound", R.string.RecordingRound));
-                    }
-                    newPrintingStringsTypes.put(key, 4);
-                } else if (pu.action instanceof TLRPC.TL_sendMessageUploadAudioAction) {
-                    if (lower_id < 0) {
-                        newPrintingStrings.put(key, LocaleController.formatString("IsSendingAudio", R.string.IsSendingAudio, getUserNameForTyping(user)));
-                    } else {
-                        newPrintingStrings.put(key, LocaleController.getString("SendingAudio", R.string.SendingAudio));
-                    }
-                    newPrintingStringsTypes.put(key, 2);
-                } else if (pu.action instanceof TLRPC.TL_sendMessageUploadVideoAction || pu.action instanceof TLRPC.TL_sendMessageRecordVideoAction) {
-                    if (lower_id < 0) {
-                        newPrintingStrings.put(key, LocaleController.formatString("IsSendingVideo", R.string.IsSendingVideo, getUserNameForTyping(user)));
-                    } else {
-                        newPrintingStrings.put(key, LocaleController.getString("SendingVideoStatus", R.string.SendingVideoStatus));
-                    }
-                    newPrintingStringsTypes.put(key, 2);
-                } else if (pu.action instanceof TLRPC.TL_sendMessageUploadDocumentAction) {
-                    if (lower_id < 0) {
-                        newPrintingStrings.put(key, LocaleController.formatString("IsSendingFile", R.string.IsSendingFile, getUserNameForTyping(user)));
-                    } else {
-                        newPrintingStrings.put(key, LocaleController.getString("SendingFile", R.string.SendingFile));
-                    }
-                    newPrintingStringsTypes.put(key, 2);
-                } else if (pu.action instanceof TLRPC.TL_sendMessageUploadPhotoAction) {
-                    if (lower_id < 0) {
-                        newPrintingStrings.put(key, LocaleController.formatString("IsSendingPhoto", R.string.IsSendingPhoto, getUserNameForTyping(user)));
-                    } else {
-                        newPrintingStrings.put(key, LocaleController.getString("SendingPhoto", R.string.SendingPhoto));
-                    }
-                    newPrintingStringsTypes.put(key, 2);
-                } else if (pu.action instanceof TLRPC.TL_sendMessageGamePlayAction) {
-                    if (lower_id < 0) {
-                        newPrintingStrings.put(key, LocaleController.formatString("IsSendingGame", R.string.IsSendingGame, getUserNameForTyping(user)));
-                    } else {
-                        newPrintingStrings.put(key, LocaleController.getString("SendingGame", R.string.SendingGame));
-                    }
-                    newPrintingStringsTypes.put(key, 3);
-                } else {
+//                if (pu.action instanceof TLRPC.TL_sendMessageRecordAudioAction) {
+//                    if (lower_id < 0) {
+//                        newPrintingStrings.put(key, LocaleController.formatString("IsRecordingAudio", R.string.IsRecordingAudio, getUserNameForTyping(user)));
+//                    } else {
+//                        newPrintingStrings.put(key, LocaleController.getString("RecordingAudio", R.string.RecordingAudio));
+//                    }
+//                    newPrintingStringsTypes.put(key, 1);
+//                } else if (pu.action instanceof TLRPC.TL_sendMessageRecordRoundAction || pu.action instanceof TLRPC.TL_sendMessageUploadRoundAction) {
+//                    if (lower_id < 0) {
+//                        newPrintingStrings.put(key, LocaleController.formatString("IsRecordingRound", R.string.IsRecordingRound, getUserNameForTyping(user)));
+//                    } else {
+//                        newPrintingStrings.put(key, LocaleController.getString("RecordingRound", R.string.RecordingRound));
+//                    }
+//                    newPrintingStringsTypes.put(key, 4);
+//                } else if (pu.action instanceof TLRPC.TL_sendMessageUploadAudioAction) {
+//                    if (lower_id < 0) {
+//                        newPrintingStrings.put(key, LocaleController.formatString("IsSendingAudio", R.string.IsSendingAudio, getUserNameForTyping(user)));
+//                    } else {
+//                        newPrintingStrings.put(key, LocaleController.getString("SendingAudio", R.string.SendingAudio));
+//                    }
+//                    newPrintingStringsTypes.put(key, 2);
+//                } else if (pu.action instanceof TLRPC.TL_sendMessageUploadVideoAction || pu.action instanceof TLRPC.TL_sendMessageRecordVideoAction) {
+//                    if (lower_id < 0) {
+//                        newPrintingStrings.put(key, LocaleController.formatString("IsSendingVideo", R.string.IsSendingVideo, getUserNameForTyping(user)));
+//                    } else {
+//                        newPrintingStrings.put(key, LocaleController.getString("SendingVideoStatus", R.string.SendingVideoStatus));
+//                    }
+//                    newPrintingStringsTypes.put(key, 2);
+//                } else if (pu.action instanceof TLRPC.TL_sendMessageUploadDocumentAction) {
+//                    if (lower_id < 0) {
+//                        newPrintingStrings.put(key, LocaleController.formatString("IsSendingFile", R.string.IsSendingFile, getUserNameForTyping(user)));
+//                    } else {
+//                        newPrintingStrings.put(key, LocaleController.getString("SendingFile", R.string.SendingFile));
+//                    }
+//                    newPrintingStringsTypes.put(key, 2);
+//                } else if (pu.action instanceof TLRPC.TL_sendMessageUploadPhotoAction) {
+//                    if (lower_id < 0) {
+//                        newPrintingStrings.put(key, LocaleController.formatString("IsSendingPhoto", R.string.IsSendingPhoto, getUserNameForTyping(user)));
+//                    } else {
+//                        newPrintingStrings.put(key, LocaleController.getString("SendingPhoto", R.string.SendingPhoto));
+//                    }
+//                    newPrintingStringsTypes.put(key, 2);
+//                } else if (pu.action instanceof TLRPC.TL_sendMessageGamePlayAction) {
+//                    if (lower_id < 0) {
+//                        newPrintingStrings.put(key, LocaleController.formatString("IsSendingGame", R.string.IsSendingGame, getUserNameForTyping(user)));
+//                    } else {
+//                        newPrintingStrings.put(key, LocaleController.getString("SendingGame", R.string.SendingGame));
+//                    }
+//                    newPrintingStringsTypes.put(key, 3);
+//                } else {
                     if (lower_id < 0) {
                         newPrintingStrings.put(key, String.format("%s %s", getUserNameForTyping(user), LocaleController.getString("IsTyping", R.string.IsTyping)));
                     } else {
                         newPrintingStrings.put(key, LocaleController.getString("Typing", R.string.Typing));
                     }
                     newPrintingStringsTypes.put(key, 0);
-                }
+//                }
             } else {
                 int count = 0;
                 String label = "";
