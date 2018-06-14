9834745f72c60eefc4e4a0d7611efb55230c24a8
Bruno Belic <brunobelic@hotmail.com>
2018-06-14 13:04:23 +0200

    Add app constants

---
+++ TMessagesProj/src/main/java/de/privalino/telegram/AppConstants.java
@@ -0,0 +1,22 @@
+package de.privalino.telegram;
+
+public class AppConstants {
+
+    //shared preferences keys
+    public static final String SHAREDRPREFS_KEY_ON_BOARDING_INFO = "onboardinfo";
+    public static final String SHAREDPREFS_KEY_USER_TYPE_SELECTED = "selected";
+    public static final String SHAREDPREFS_KEY_IS_PARENT = "is_parent";
+    public static final String SHAREDPREFS_KEY_EMAIL = "email";
+    public static final String SHAREDPREFS_KEY_CHILDREN = "children";
+    public static final String SHAREDPREFS_KEY_PARENTS = "parents";
+
+    //permision request and result codes
+    public static final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 314;
+    public static final int RESULT_PICK_CONTACT = 159;
+
+    //intent extras keys
+    public static final String INTENT_EXTRA_KEY_FROM_SETTINGS = "from_settings";
+    public static final String INTENT_EXTRA_KEY_FROM_INTRO = "fromIntro";
+    public static final String INTENT_EXTRA_KEY_IS_PARENT = "isParent";
+
+}
