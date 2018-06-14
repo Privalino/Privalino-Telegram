849f0a0cb0ee8cb73a93fdfba2a66baa1e7e153a
Bruno Belic <brunobelic@hotmail.com>
2018-06-14 13:07:10 +0200

    Modify an adapter to add a new item in the app main menu

--- TMessagesProj/src/main/java/org/telegram/ui/Adapters/DrawerLayoutAdapter.java
+++ TMessagesProj/src/main/java/org/telegram/ui/Adapters/DrawerLayoutAdapter.java
@@ -117,8 +117,9 @@ public class DrawerLayoutAdapter extends RecyclerListView.SelectionAdapter {
             //items.add(new Item(10, LocaleController.getString("Calls", R.string.Calls), R.drawable.menu_calls));
         }
         items.add(new Item(6, LocaleController.getString("InviteFriends", R.string.InviteFriends), R.drawable.menu_invite));
-        items.add(new Item(7, LocaleController.getString("Settings", R.string.Settings), R.drawable.menu_settings));
-        items.add(new Item(8, LocaleController.getString("TelegramFaq", R.string.TelegramFaq), R.drawable.menu_help));
+        items.add(new Item(7, mContext.getString(R.string.connect_parent_with_child), R.drawable.ic_child));
+        items.add(new Item(8, LocaleController.getString("Settings", R.string.Settings), R.drawable.menu_settings));
+        items.add(new Item(9, LocaleController.getString("TelegramFaq", R.string.TelegramFaq), R.drawable.menu_help));
 
     }
 
